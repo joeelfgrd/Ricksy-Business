ecfe0e679d7b5d9767fedadc4eb9949a4ad7df5b
Joel Figueirido <joelfigueiridomolares@gmail.com>
2024-04-04 19:14:21 +0200

    feat(CreditCard): Añadir clase CreditCard

---
+++ app/src/main/java/org/example/CreditCard.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class CreditCard implements PayMethod{
+
+   private final String owner;
+    private final String number;
+    private double credit = 3000;
+    private final String symbol = "EZI";
+
+        public CreditCard(String owner, String number) {
+            this.owner = owner;
+            this.number = number;
+        }
+        
+        
+        public boolean pay(double cash) {
+            boolean canPay = cash <= credit;
+            credit = canPay ? credit - cash : credit;
+            return canPay;
+        }
+        
+        public String number() {
+            return this.number;
+        }
+
+        String cardOwner() {
+            return this.owner;
+        }
+        
+        double credit() {
+            return this.credit;
+        }
+
+        @Override
+        public String toString() {
+            return "owner: " + owner + ", /nnumber: " + number + ", /ncredit: " + credit  + symbol;
+        }
+
+
+    }
