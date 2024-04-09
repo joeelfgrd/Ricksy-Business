cbd4e59d0a67cdee13cc71606c1fbd0a367964a0
Joel Figueirido <joelfigueiridomolares@gmail.com>
2024-04-09 20:50:52 +0200

    fix(CrystalExpender): modificacion de la  clase CrystalExpender añadiendo los metodos

--- app/src/main/java/org/example/CrystalExpender.java
+++ app/src/main/java/org/example/CrystalExpender.java
@@ -1,5 +1,28 @@
 package org.example;
 
-public class CrystalExpender {
+public class CrystalExpender implements GuestDispatcher{
+    private int stock=0;
+    private double itemCost=0d;
+    public CrystalExpender(int stock,double itemCost){
+        this.stock=stock;
+        this.itemCost=itemCost;
+    }
     
+    @Override
+    public void dispatch(PayMethod paymethod){
+        if(stock> 0  && paymethod.pay(itemCost)){
+            stock -=1;
+        }
+    }
+    public int stock(){
+        return this.stock;
+    }
+    @Override
+    public String toString(){
+        return "stock" + this.stock + 
+        "/ncost" + this.itemCost;
+
+    }
 }
+
+
